e279c0278ad582ec6b62e251cea8d389aedd4e17
Caitlyn <craigoda+caitlyn@gmail.com>
2021-02-08 07:45:39 -0800

    video 136 3:14

--- animal.dart
+++ animal.dart
@@ -1,31 +1,37 @@
 class Animal {
+  const Animal({required this.age});
+  final int age;
+
   void sleep() => print('sleep');
 }
 
 class Dog extends Animal {
+ Dog({required int age}) : super(age: age);
+
   void bark() => print('bark');
+  @override
+  void sleep() {
+super.sleep();
+print('sleep some more');
+  }
 }
 
 class Cow extends Animal {
+   Cow({required int age}) : super(age: age);
+
   void moo() => print('moo');
 }
 
 class CleverDog extends Dog {
+   CleverDog({required int age}) : super(age: age);
+
   void catchBall() => print('catch');
 }
 void main(){
-  final animal = Animal();
-  animal.sleep();
+  final animal = Animal(age: 10);
+ // animal.sleep();
 
-  final dog = Dog();
-  dog.bark();
+  final dog = Dog(age: 39);
   dog.sleep();
 
-  final cow = Cow();
-  cow.moo();
-  cow.sleep();
-
-  final cleverDog = CleverDog();
-  cleverDog.catchBall();
-  cleverDog.sleep();
 }
